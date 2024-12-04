8b788c22263189d8f37847c3c49daf33592c5811
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-04 16:18:28 +0800

    change query

--- Server_Side/Enrollment/enrollment-backend.php
+++ Server_Side/Enrollment/enrollment-backend.php
@@ -5,7 +5,7 @@ class Show_admission extends DatabaseConnection{
     public function Show_Pending_admission(){
 
     $admissiom_list = "SELECT * FROM (SELECT * FROM tbl_admission order by date ASC ) tbl_admission 
-    JOIN (SELECT * FROM tbl_student_info WHERE Student_Type = 'NE') tbl_student_info on tbl_admission.id = tbl_student_info.student_id GROUP BY tbl_admission.id";
+    JOIN (SELECT * FROM tbl_student_info WHERE Student_Type = 'NE') tbl_student_info on tbl_admission.id = tbl_student_info.student_id ";
     $stmt = $this->conn->prepare($admissiom_list);
     $stmt->execute(); 
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
