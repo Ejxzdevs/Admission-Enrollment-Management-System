bf72b1dd610363f2eae17645a327d0cb3d2376e7
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-04 17:15:29 +0800

    fix container , padding , font-size and remove socmed icons

--- Client_Side/Footer/footer.php
+++ Client_Side/Footer/footer.php
@@ -1,7 +1,6 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-<div class="row" style="background-color: transparent; flex-wrap: wrap; ">
+<div  class="row" style="background-color: transparent; flex-wrap: wrap; height: 200px; ">
   <div class="col-sm-6 mb-3 mb-sm-0  ">
     <div class="card" style="background-color: transparent; border:none;">
       <div class="card-body">
@@ -9,7 +8,6 @@
         <label class="label" ><img class="icons"  src="../../Icons/placeholder.png" alt=""><p>IPO Road, Barangay Minuyan Proper City of San Jose Del Monte, Bulacan</p></label>
         <label class="label"><img class="icons" src="../../Icons/phone.png" alt=""><p>0926-234-1234</p></label>
         <label class="label"><img class="icons" src="../../Icons/gmail.png" alt=""><p>bestlink@gmail.com</p></label>
-        <label class="label social-icons"><a href=""><img src="../../Icons/facebook.png" alt="facebook"></a><a href=""><img src="../../Icons/twitter.png" alt="twitter"></a><a href=""><img src="../../Icons/instagram.png" alt="instagram."></a> </label>
       </div>
     </div>
   </div>
@@ -17,20 +15,20 @@
     <div class="card" style="background-color: transparent; border:none;">
       <div class="card-body">
         <h5 class="card-title title-links" style="color:white">Quick Links</h5>
-        <ul class="Links">
-        <li><a href="../Home/home.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"> Home</a></li>
-          <li><a href="../About/about.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"> About Us</a></li>
-          <li><a href="../Courses/courses.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"> Courses</a></li>
-          <li><a href="../Inquire/inquire.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"> Inquire</a></li>
-          <li><a href="../Public_Notice/public.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"> Public Notice</a></li>
+        <ul class="Links pt-1">
+        <li><a style="font-size: 14px;" href="../Home/home.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"><span>Home</span></a></li>
+          <li><a style="font-size: 14px;" href="../About/about.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"><span>About Us</span> </a></li>
+          <li><a style="font-size: 14px;" href="../Courses/courses.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"><span>Courses</span> </a></li>
+          <li><a style="font-size: 14px;" href="../Inquire/inquire.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"><span>Inquire</span> </a></li>
+          <li><a style="font-size: 14px;" href="../Public_Notice/public.php"><img class="links-arrow" src="../../Icons/right-arrow.svg"><span>Public Notice</span> </a></li>
         </ul>
       </div>
     </div>
   </div>
 </div>
 
-<div class="dev-by">
-  <p>© Copyright BCP. All Rights Reserved</p>
+<div class="dev-by p-0 m-0">
+  <p class="p-0 m-0" >© Copyright BCP. All Rights Reserved</p>
   <label>Developed by Ejhay Gofredo</label>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
