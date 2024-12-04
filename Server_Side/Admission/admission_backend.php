7b3a14e0a1861c1c2371b4b9731284c12d35150e
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-04 22:11:10 +0800

    add transfer close connection

--- Server_Side/Admission/admission_backend.php
+++ Server_Side/Admission/admission_backend.php
@@ -19,38 +19,18 @@ class Show_admission extends DatabaseConnection{
     $stmt = $this->conn->prepare($admissiom_list);
     $stmt->execute(); 
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-    
-    return $data;
     $this->conn = null;
-      
+    return $data;
+   
     }
-
     public function Search_Pending_admission($search_name){
 
         $admissiom_list = "SELECT * FROM (SELECT * FROM tbl_admission ) tbl_admission 
-        JOIN (SELECT * FROM tbl_student_info WHERE firstName LIKE '$search_name%' AND Student_Type = 'NE' or lastName LIKE '$search_name%' AND Student_Type = 'NE' ) tbl_student_info on tbl_admission.id = tbl_student_info.student_id GROUP BY tbl_admission.id";
-
+        JOIN (SELECT * FROM tbl_student_info WHERE firstName LIKE '$search_name%' AND Student_Type = 'NE' or lastName LIKE '$search_name%' AND Student_Type = 'NE' ) tbl_student_info on tbl_admission.id = tbl_student_info.student_id ";
         $stmt = $this->conn->prepare($admissiom_list);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $data;
-
-    
         $this->conn = null;
-          
+        return $data;          
     }
-
-
-    
-
-
-    
 }
-
-
-
-
-?>
