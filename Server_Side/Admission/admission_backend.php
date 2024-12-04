1aa0ad314575272c9705c511fc689bcc1fe10705
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-04 15:39:31 +0800

    modified query

--- Server_Side/Admission/admission_backend.php
+++ Server_Side/Admission/admission_backend.php
@@ -4,8 +4,19 @@ include "../../Connection/connection.php";
 class Show_admission extends DatabaseConnection{
     public function Show_Pending_admission(){
 
-    $admissiom_list = "SELECT * FROM (SELECT * FROM tbl_admission order by date ASC ) tbl_admission 
-    JOIN (SELECT * FROM tbl_student_info WHERE Student_Type = 'NE') tbl_student_info on tbl_admission.id = tbl_student_info.student_id GROUP BY tbl_admission.id";
+    $admissiom_list = "SELECT 
+    tbl_admission.*,
+    tbl_student_info.*
+FROM 
+    tbl_admission 
+JOIN 
+    tbl_student_info 
+    ON tbl_admission.id = tbl_student_info.student_id 
+WHERE 
+    tbl_student_info.Student_Type = 'NE'
+ORDER BY 
+    tbl_admission.date ASC;
+";
     $stmt = $this->conn->prepare($admissiom_list);
     $stmt->execute(); 
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
