8490acab8c2e889a5b6fe0aee3c8b0027027af1a
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-05 15:45:42 +0800

    implement dynamic search bar

--- Head_admin/Subject/subject_backend.php
+++ Head_admin/Subject/subject_backend.php
@@ -2,6 +2,33 @@
 include "../../Connection/connection.php";
 
 class Subject extends DatabaseConnection{
+
+     // READ SUBJECT
+     public function Show_Subject(){
+
+        $show_course = "SELECT * FROM `tbl_subject`";
+        $stmt = $this->conn->prepare($show_course);
+        $stmt->execute(); 
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->conn = null;
+        return $data;
+        
+    }
+
+    // SERCH SUBJECT
+
+    public function Search_Subject($name){
+
+        $show_course = "SELECT * FROM `tbl_subject` WHERE subject_name LIKE '%$name%'";
+        $stmt = $this->conn->prepare($show_course);
+        $stmt->execute(); 
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->conn = null;
+        return $data;
+        
+    }
+
+    // Insert Subject
     public function Insert_Subject($sub_name,$sub_unit,$sub_code,$sub_description){
 
         $insert_subject = "INSERT INTO `tbl_subject` (`subject_name`,`subject_code`,`subject_description`, `subject_unit`) VALUES (:sub_name,:sub_code,:sub_description,:sub_unit)";
@@ -10,25 +37,13 @@ class Subject extends DatabaseConnection{
         $stmt->bindParam(':sub_code', $sub_code);
         $stmt->bindParam(':sub_unit', $sub_unit);
         $stmt->bindParam(':sub_description', $sub_description);
-        
+        $this->conn = null;
         $stmt->execute();
         return 200;
-        $this->conn = null;
-          
+   
     }
 
-    public function Show_Subject(){
-
-        $show_course = "SELECT * FROM `tbl_subject`";
-        $stmt = $this->conn->prepare($show_course);
-        $stmt->execute(); 
-        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $data;
-        $this->conn = null;
-          
-    }
-
+    // UPDATE SUBJECT
     public function update_Subject($sub_id,$sub_name,$sub_unit,$sub_code,$sub_description){
 
         $update_subject = "UPDATE `tbl_subject` SET `subject_name`=:sub_name,`subject_code`=:sub_code,`subject_description`=:sub_description,`subject_unit`=:sub_unit WHERE subject_id = :sub_id ";
@@ -39,37 +54,20 @@ class Subject extends DatabaseConnection{
         $stmt->bindParam(':sub_unit', $sub_unit);
         $stmt->bindParam(':sub_description', $sub_description);
         $stmt->execute(); 
-
-        return 200;
         $this->conn = null;
-          
+        return 200;
     }
 
-    
+    // DELETE SUBJECT
     public function delete_Subject($id){
 
         $delete_subject = "DELETE FROM `tbl_subject` WHERE subject_id = :id";
         $stmt = $this->conn->prepare($delete_subject);
         $stmt->bindParam(':id',$id);
         $stmt->execute(); 
-
-        return 200;
         $this->conn = null;
-          
+        return 200;
+       
     }
     
-
- 
-
-
-    
 }
-
-
-
-
-
-
-
-
-?>
