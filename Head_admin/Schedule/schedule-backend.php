06ffcf49f6a05e18008204ef3713bcecdb732b14
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-05 12:54:09 +0800

    added Search Schedule Method for Filter

--- Head_admin/Schedule/schedule-backend.php
+++ Head_admin/Schedule/schedule-backend.php
@@ -2,16 +2,15 @@
 include "../../Connection/connection.php";
 
 class Schedule extends DatabaseConnection{
+    
+    // Insert Schedule
     public function Insert_Schedule($name,$day,$subject,$time_in,$time_out){
 
         $insert_sched = "INSERT INTO `tbl_schedule`(`schedule_name`) VALUES (:name)";
         $stmt = $this->conn->prepare($insert_sched);
         $stmt->bindParam(':name', $name);
-       
-        
         $stmt->execute();
         $id = $this->conn->lastInsertId();
-
 
         $insert_day = "INSERT INTO `tbl_schedule_day`(`schedule_id`,`subject`, `day`, `time_in`, `time_out`) VALUES (:id,:subject,:day,:time_in,:time_out)";
         $stmt2 = $this->conn->prepare($insert_day);
@@ -22,40 +21,40 @@ class Schedule extends DatabaseConnection{
             $stmt2->bindParam(':subject', $subject[$i]);
             $stmt2->bindParam(':time_in', $time_in[$i]);
             $stmt2->bindParam(':time_out', $time_out[$i]);
-        
-            // Execute the query
             $stmt2->execute();
         }
-
-        return 200 ;
-       
-          
+        return 200 ; 
     }
 
+    // View Schedule
     public function Show_Schedule(){
-
         $show_sched= " SELECT * FROM tbl_schedule ";
         $stmt = $this->conn->prepare($show_sched);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
         return $data;
-        
-          
     }
 
+    // Search Schedule
+    Public function Search_Schedule($name){
+        $show_sched= " SELECT * FROM tbl_schedule Where schedule_name LIKE '%$name%' ";
+        $stmt = $this->conn->prepare($show_sched);
+        $stmt->execute(); 
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+
+    // Show Subject For Modal Create Schedule
     public function Show_Subject(){
 
         $show_sched= " SELECT * FROM tbl_subject ";
         $stmt = $this->conn->prepare($show_sched);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
         return $data;
-        
-          
     }
 
+    // For Update Course
     public function Update_Course($course_title,$course_description,$course_id){
 
         $update_course = "UPDATE `tbl_course` SET `course_name`=:course_title,`course_description`=:course_description WHERE course_id = :course_id";
@@ -64,13 +63,10 @@ class Schedule extends DatabaseConnection{
         $stmt->bindParam(':course_description', $course_description);
         $stmt->bindParam(':course_title', $course_title);
         $stmt->execute(); 
-
         return 200;
-        
-          
     }
 
-    
+    // For Delete Schedule
     public function Delete_Schedule($id){
 
         $delete_schedule = "DELETE FROM `tbl_schedule` WHERE schedule_id = :id";
@@ -84,10 +80,9 @@ class Schedule extends DatabaseConnection{
         $stmt2->execute(); 
 
         return 200;
-        
-          
     }
 
+    // For Viewing Schedule
     public function View_Schedule($id){
 
         $show_sched= " SELECT * FROM (SELECT * FROM tbl_schedule where schedule_id = :id) tbl_schedule JOIN
@@ -107,12 +102,10 @@ class Schedule extends DatabaseConnection{
         $stmt->execute(); 
         
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
         return $data;
-        
-          
     }
 
+     // For Updating Schedule
     public function Update_Schedule($id,$time_in,$time_out){
 
         $time_one = date('H:i:s.u',strtotime($time_in));
@@ -126,18 +119,7 @@ class Schedule extends DatabaseConnection{
         $stmt->execute(); 
         
         return 200;
-        
-
-
-
-
     }
-    
-
- 
-
-
-    
 }
 
 
