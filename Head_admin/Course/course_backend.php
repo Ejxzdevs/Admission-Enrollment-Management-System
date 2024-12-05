a156471ed8355247dff4a64ac88faa72015bdd20
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-05 15:53:25 +0800

    added search method for filter course

--- Head_admin/Course/course_backend.php
+++ Head_admin/Course/course_backend.php
@@ -2,31 +2,41 @@
 include "../../Connection/connection.php";
 
 class Course extends DatabaseConnection{
-    public function Insert_Course($title,$description){
 
-        $insert_course = "INSERT INTO `tbl_course`(`course_name`, `course_description`) VALUES (:title,:description)";
-        $stmt = $this->conn->prepare($insert_course);
-        $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':title', $title);
-        
-        $stmt->execute();
-        return 200;
-        $this->conn = null;
-          
-    }
-
-    public function Show_Course(){
+      // SHOW ALL COURSE
+      public function Show_Course(){
 
         $show_course = "SELECT * FROM `tbl_course`";
         $stmt = $this->conn->prepare($show_course);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $data;
         $this->conn = null;
-          
+        return $data;    
     }
 
+    // SEARCH SPECIFIC COURSE
+    public function Search_Course($name){
+
+        $show_course = "SELECT * FROM `tbl_course` WHERE course_name LIKE '%$name%'";
+        $stmt = $this->conn->prepare($show_course);
+        $stmt->execute(); 
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->conn = null;
+        return $data;    
+    }
+
+    // INSERT COURSE
+    public function Insert_Course($title,$description){
+        $insert_course = "INSERT INTO `tbl_course`(`course_name`, `course_description`) VALUES (:title,:description)";
+        $stmt = $this->conn->prepare($insert_course);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':title', $title);
+        $this->conn = null;
+        $stmt->execute();
+        return 200;
+    }
+
+    // UPDATE COURSE
     public function Update_Course($course_title,$course_description,$course_id){
 
         $update_course = "UPDATE `tbl_course` SET `course_name`=:course_title,`course_description`=:course_description WHERE course_id = :course_id";
@@ -35,37 +45,18 @@ class Course extends DatabaseConnection{
         $stmt->bindParam(':course_description', $course_description);
         $stmt->bindParam(':course_title', $course_title);
         $stmt->execute(); 
-
-        return 200;
         $this->conn = null;
-          
+        return 200;
     }
 
-    
+    // DELETE COURSE
     public function delete_Course($id){
 
         $delete_course = "DELETE FROM `tbl_course` WHERE course_id = :id";
         $stmt = $this->conn->prepare($delete_course);
         $stmt->bindParam(':id',$id);
         $stmt->execute(); 
-
-        return 200;
         $this->conn = null;
-          
+        return 200;
     }
-    
-
- 
-
-
-    
 }
-
-
-
-
-
-
-
-
-?>
