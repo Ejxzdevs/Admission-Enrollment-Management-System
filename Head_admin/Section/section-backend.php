5ee5df921bc388b37fb585907742ac2bb07b1573
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-06 10:52:01 +0800

    modified show and search statement from JOIN to LEFT JOIN

--- Head_admin/Section/section-backend.php
+++ Head_admin/Section/section-backend.php
@@ -3,47 +3,41 @@ include "../../Connection/connection.php";
 
 class Section extends DatabaseConnection{
     
+    // SHOW SECTIONS
     public function Show_Section(){
 
-        $show_section= "SELECT * FROM ( SELECT * FROM tbl_section ) tbl_section JOIN 
+        $show_section= "SELECT * FROM ( SELECT * FROM tbl_section ) tbl_section LEFT JOIN 
          ( SELECT * FROM tbl_schedule ) tbl_schedule on  tbl_section.schedule_id = tbl_schedule.schedule_id ";
         $stmt = $this->conn->prepare($show_section);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $data;
         $this->conn = null;
-          
+        return $data;
     }
-
+    // SEARCH SPECIFIC SECTION
     public function Search_Section($search_name){
 
-
-        $search_inquiries = " SELECT * FROM ( SELECT * FROM tbl_section WHERE section_name LIKE '$search_name%') tbl_section JOIN 
+        $search_inquiries = " SELECT * FROM ( SELECT * FROM tbl_section WHERE section_name LIKE '$search_name%') tbl_section LEFT JOIN 
          ( SELECT * FROM tbl_schedule ) tbl_schedule on  tbl_section.schedule_id = tbl_schedule.schedule_id "; 
         $stmt = $this->conn->prepare($search_inquiries);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-    return $data;
-    $this->conn = null;
-          
+        $this->conn = null;
+        return $data;   
     }
     
-
-
+    // SHOW SCHEDULE FOR OPTIONS
     public function Show_Schedule(){
 
         $show_sched= " SELECT * FROM tbl_schedule ";
         $stmt = $this->conn->prepare($show_sched);
         $stmt->execute(); 
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $data;
         $this->conn = null;
-          
+        return $data;     
     }
 
+    // INSERT SECTION
     public function Insert_Section($name,$sched,$room){
 
         $insert_section = "INSERT INTO `tbl_section`(`schedule_id`,`section_name`,`room`) VALUES (:sched,:name,:room)";
@@ -52,11 +46,11 @@ class Section extends DatabaseConnection{
         $stmt->bindParam(':sched', $sched);
         $stmt->bindParam(':room', $room);
         $stmt->execute();
-        return 200;
         $this->conn = null;
-
+        return 200;
     }
 
+    // UPDATE SECTION
     public function Update_Section($name,$sched,$id,$room){
 
         $update_section = "UPDATE `tbl_section` SET `schedule_id`= :sched ,`section_name`= :name , `room` = :room WHERE section_id = :id";
@@ -66,37 +60,20 @@ class Section extends DatabaseConnection{
         $stmt->bindParam(':room', $room);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-        return 200;
         $this->conn = null;
-
-
+        return 200;
+       
     }
-
+    // DELETE SECTION
     public function Delete_Section($id){
 
         $update_section = "DELETE FROM `tbl_section` WHERE section_id = :id ";
         $stmt = $this->conn->prepare($update_section);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-        return 200;
         $this->conn = null;
-
-
+        return 200;
+    
     }
 
-   
-
- 
-
-
-    
 }
-
-
-
-
-
-
-
-
-?>
