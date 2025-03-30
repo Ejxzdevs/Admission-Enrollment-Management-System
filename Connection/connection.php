60939e4d75819f6769eb1c0368276e2a0a672dbf
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-03-30 22:46:31 +0800

    fix: Add condition to include correct autoload path based on script location

--- Connection/connection.php
+++ Connection/connection.php
@@ -1,13 +1,26 @@
-<?php 
+<?php
+if(basename($_SERVER['PHP_SELF']) == 'Login.php') {
+    require_once '../vendor/autoload.php';
+}else{
+    require_once '../../vendor/autoload.php';
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '../../');
+$dotenv->load();
 
 class DatabaseConnection {
-    private $host = "localhost";
-    private $dbname = "enrollment_system";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $dbname;
+    private $username;
+    private $password;
     public $conn;
 
     public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->dbname = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+        
         try {
             $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}",$this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,4 +29,4 @@ class DatabaseConnection {
         }
     }
 }
-
+ 
